7b2cb1cc2a5f9bb6fe953345ee80809e38562ced
Robert <packleader@dumbdogentertainment.com>
2018-01-14 21:29:44 -0600

    -- towers can shoot;

---
+++ Reload/Assets/Scripts/ShellBehavior.cs
@@ -0,0 +1,25 @@
+﻿namespace DumbDogEntertainment
+{
+    using UnityEngine;
+
+    public class ShellBehavior : MonoBehaviour
+    {
+        public float TimeUntilDestroy = 2f;
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+            if(this.TimeUntilDestroy <= 0)
+            {
+                Destroy(this.gameObject);
+            }
+
+            this.transform.position -= new Vector3(0, 0, 0.1f);
+            this.TimeUntilDestroy -= Time.deltaTime;
+        }
+    }
+}
--- Reload/Assets/Scripts/TowerBehavior.cs
+++ Reload/Assets/Scripts/TowerBehavior.cs
@@ -1,14 +1,63 @@
-﻿using UnityEngine;
-
-public class TowerBehavior : MonoBehaviour
+﻿namespace DumbDogEntertainment
 {
-    void Awake()
+    using System;
+    using System.Collections;
+    using UnityEngine;
+
+    public class TowerBehavior : MonoBehaviour
     {
+        public float maxEnergy = 100f;
+        public float currentEnergy;
+        public float energyRechargeRate = 1f;
+        public float energyRechargeAmount = 2f;
+        public float energyRechargeCooldown;
 
-    }
+        public GameObject shellPrefab;
+        public float fireRate = 0.5f;
+        public float fireCooldown;
 
-    void Update()
-    {
+        Transform muzzleTransform;
 
+        void Awake()
+        {
+            this.currentEnergy = this.maxEnergy;
+            this.energyRechargeCooldown = this.energyRechargeRate;
+
+            this.muzzleTransform = transform.Find("barrel");
+            this.fireCooldown = this.fireRate;
+        }
+
+        void Update()
+        {
+            #region Recharge
+
+            if (this.energyRechargeCooldown <= 0)
+            {
+                this.currentEnergy += this.energyRechargeRate;
+                this.currentEnergy = Mathf.Clamp(this.currentEnergy, 0f, this.maxEnergy);
+                this.energyRechargeCooldown = this.energyRechargeRate;
+            }
+
+            this.energyRechargeCooldown -= Time.deltaTime;
+
+            #endregion
+
+            if (this.fireCooldown <= 0)
+            {
+                ////GameObject shellGameObject = (GameObject)Instantiate(
+                ////    this.shellPrefab,
+                ////    muzzleTransform.position,
+                ////    muzzleTransform.rotation);
+
+                Instantiate(
+                    this.shellPrefab,
+                    muzzleTransform.position,
+                    muzzleTransform.rotation);
+
+                this.fireCooldown = this.fireRate;
+            }
+
+            this.fireCooldown -= Time.deltaTime;
+        }
     }
 }
