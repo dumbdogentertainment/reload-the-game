08feb7bdeba4a74ce86d02e06e974a0c03b5e7e0
Robert <packleader@dumbdogentertainment.com>
2018-01-21 13:52:00 -0600

    -- player boost energy experiment;

---
+++ Reload/Assets/Scripts/CameraFacingBillboard.cs
@@ -0,0 +1,14 @@
+﻿namespace DumbDogEntertainment
+{
+    using UnityEngine;
+
+    public class CameraFacingBillboard : MonoBehaviour
+    {
+        void Update()
+        {
+            this.transform.LookAt(
+                this.transform.position + Camera.main.transform.rotation * Vector3.forward,
+                Camera.main.transform.rotation * Vector3.up);
+        }
+    }
+}
--- Reload/Assets/Scripts/Player.cs
+++ Reload/Assets/Scripts/Player.cs
@@ -1,6 +1,9 @@
 ﻿namespace DumbDogEntertainment
 {
+    using System.Collections;
+
     using UnityEngine;
+    using UnityEngine.UI;
 
     [RequireComponent(typeof(Rigidbody))]
     public class Player : MonoBehaviour
@@ -21,9 +24,21 @@
 
         #region Properties and variables
 
-        public float playerSpeed = 5f;
-        private Rigidbody playerRigidbody;
+        public float energy;
+        public float maximumEnergy = 100f;
+        public float boostedMaximumEnergy = 150f;
+        private Image energyImage;
+        private Image boostedEnergyImage;
 
+        public float speed = 5f;
+        public float boostedSpeed = 8f;
+
+        [SerializeField]
+        private bool isCurrentlyBoosted = false;
+        [SerializeField]
+        private bool isCurrentlyEnergizing = false;
+
+        private Rigidbody playerRigidbody;
         private float horizontalMovement;
 
         #endregion
@@ -31,19 +46,40 @@
         void Start()
         {
             this.playerRigidbody = GetComponent<Rigidbody>();
+
+            this.energyImage = this.transform
+                .Find("EnergyCanvas")
+                .Find("energy_image")
+                .GetComponent<Image>();
+            this.boostedEnergyImage = this.transform
+                .Find("EnergyCanvas")
+                .Find("boosted_energy_image")
+                .GetComponent<Image>();
+
+            this.energy = Random.Range(50f, this.maximumEnergy);
         }
 
         void Update()
         {
-            var debugRayStartPosition = new Vector3(
-                this.transform.position.x,
-                this.transform.position.y,
-                this.transform.position.z);
+            // regen energy
+            this.energy = Mathf.Clamp(this.energy + 0.005f, 0, this.maximumEnergy);
 
-            Debug.DrawRay(
-                debugRayStartPosition,
-                this.transform.TransformDirection(Vector3.forward) * 5,
-                Color.red);
+            // degen boosted energy
+            if (this.isCurrentlyBoosted)
+            {
+                this.energy -= 0.03f;
+            }
+
+            UpdateEnergyCanvas();
+        }
+
+        void UpdateEnergyCanvas()
+        {
+            this.energyImage.fillAmount = this.isCurrentlyBoosted ?
+                this.energy / this.boostedMaximumEnergy :
+                this.energy / this.maximumEnergy;
+
+            this.boostedEnergyImage.enabled = this.isCurrentlyBoosted;
         }
 
         void FixedUpdate()
@@ -53,10 +89,8 @@
 
         void LateUpdate()
         {
-            Vector3 desiredMovement = this.playerSpeed * new Vector3(
-                this.horizontalMovement,
-                0,
-                0);
+            Vector3 desiredMovement = (this.isCurrentlyBoosted ? this.boostedSpeed : this.speed) *
+                new Vector3(this.horizontalMovement, 0, 0);
 
             this.playerRigidbody.velocity = desiredMovement;
 
@@ -80,7 +114,13 @@
 
         public void EnergizeNearestTower()
         {
+            if (this.isCurrentlyEnergizing)
+            {
+                return;
+            }
+
             Debug.Log("Energizing nearest tower ...");
+            StartCoroutine(GiveEnergy());
         }
 
         public void RepairNearestTower()
@@ -90,7 +130,34 @@
 
         public void EnergizeSelf()
         {
+            if (this.isCurrentlyBoosted)
+            {
+                return;
+            }
+
             Debug.Log("Giving myself a quick energy boost ...");
+            StartCoroutine(BoostSelf());
+        }
+
+        /// <summary>
+        /// Boost self .. gives additional energy (up to a certain maximum)
+        /// that degrades over time .. also, grants increased speed.
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerator BoostSelf()
+        {
+            this.isCurrentlyBoosted = true;
+            this.energy = Mathf.Clamp(this.energy + 25f, 0, this.boostedMaximumEnergy);
+            yield return new WaitForSeconds(4);
+            this.isCurrentlyBoosted = false;
+        }
+
+        private IEnumerator GiveEnergy()
+        {
+            this.isCurrentlyEnergizing = true;
+            this.energy = Mathf.Clamp(this.energy - 15, 0, this.boostedMaximumEnergy);
+            yield return new WaitForSeconds(2);
+            this.isCurrentlyEnergizing = false;
         }
     }
 }
